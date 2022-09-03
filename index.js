3a4785784b6f2a1c821db397ee6b32b092a8a62b
silverio huarcaya <corwilgi@gmail.com>
2022-09-03 18:26:44 -0500

    Initialize commit

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+<<<<<<< HEAD
 //prueba
+=======
+const { log } = require("console");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri =
+  "mongodb+srv://admin:pUAfptazvppXgs8n@silveriohuarcaya.prgzdly.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect(async (err) => {
+  if (err) {
+    console.error("Not Connected");
+    return;
+  }
+
+  const collection = client.db("agenda").collection("users");
+
+  // console.log("Collection Conected: ", collection);
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents: ", findResult);
+
+  // perform actions on the collection object
+  client.close();
+});
+>>>>>>> Initialize commit
